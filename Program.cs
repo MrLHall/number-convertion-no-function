27365f4b20bf11362602f44bb8aa3da7e155163c
Lang Hall <lhall@heckgrammar.co.uk>
2024-06-18 10:56:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_convertion_simple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the denary number you want to convert");
+            long DenaryNum = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the base of number system you want to convert the denary number to");
+            int BaseNum = Convert.ToInt16(Console.ReadLine());
+            string convertedNum = "";
+            while (DenaryNum > 0)
+            {
+                convertedNum = convertedNum.Insert(0, Convert.ToString(DenaryNum % BaseNum));
+                DenaryNum = (DenaryNum / BaseNum);
+            }
+            Console.WriteLine(convertedNum);
+        }
+    }
+}
